d1b4dfabca6488c9edd98d2969dce51886c878be
Paulo <paulo.oliveira438@etec.sp.gov.br>
2018-11-09 19:52:33 -0200

    Criação da nova classe Produto, dois atributos e usei os metodos get e set

---
+++ habbibs/src/br/com/habbibs/Produto.java
@@ -0,0 +1,29 @@
+
+package br.com.habbibs;
+
+
+public class Produto {
+    
+    public String name;
+    public double preco;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    
+    
+    
+}
