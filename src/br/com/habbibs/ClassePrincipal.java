759d3e88b8a0da28aa81b3ae585e8dac641fb311
Paulo <paulo.oliveira438@etec.sp.gov.br>
2018-11-23 20:27:38 -0200

    Criei nova classe chamada "ClassePrincipal" e criei um novo metodo

---
+++ src/br/com/habbibs/ClassePrincipal.java
@@ -0,0 +1,7 @@
+
+package br.com.habbibs;
+
+public class ClassePrincipal {
+    public static void main(String[] args){
+    }
+}
--- src/br/com/habbibs/Main.java
+++ src/br/com/habbibs/Main.java
@@ -4,6 +4,6 @@ package br.com.habbibs;
 public class Main {
     
     public static void main(String[] args){
-        System.out.println("Sistema criado com sucesso");
+        System.out.println("Sistemmaina criado com sucesso");
     }
 }
