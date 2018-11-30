36550f8f3ef4d7e4aa8695c3d1891c1fbafc0b27
LAB_ETECIA <LAB_ETECIA@10.67.96.126>
2018-11-30 20:18:40 -0200

    Fiz alterações no repositório habbibs

--- src/br/com/habbibs/Main.java
+++ src/br/com/habbibs/Main.java
@@ -1,9 +1,25 @@
 
 package br.com.habbibs;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     
     public static void main(String[] args){
-        System.out.println("Sistemmaina criado com sucesso");
+        System.out.println("Sistema inciado com sucesso");
+        
+        List<Produto> produtos;
+        produtos = new ArrayList<>();
+        
+        Produto kibe = new Produto("kibe", 4,0);
+        Produto esfirra = new Produto("Esfirra", 5,0);
+        
+        produtos.add(kibe);
+        produtos.add(esfirra);
+        
+        produtos.forEach((produto) -> {
+            System.out.println(produto.getNome());
+        });
     }
 }
--- src/br/com/habbibs/Produto.java
+++ src/br/com/habbibs/Produto.java
@@ -7,6 +7,10 @@ public class Produto {
     public String name;
     public double preco;
 
+    Produto(String kibe, int i, int i0) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
     public void setName(String name) {
         this.name = name;
     }
@@ -21,6 +25,10 @@ public class Produto {
 
     public double getPreco() {
         return preco;
+    }
+
+    boolean getNome() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     
